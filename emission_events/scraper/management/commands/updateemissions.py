ded8eafeb60cc813c84a294659219b7fe70c3af9
Marcos Vanetta <marcosvanetta@gmail.com>
2014-09-09 18:14:08 -0500

    Batch updaters task

---
+++ emission_events/scraper/management/commands/updateemissions.py
@@ -0,0 +1,17 @@
+from django.core.management.base import NoArgsCommand
+from emissions.models import PageHTML
+from scraper.url_builder import URLBuilder
+from scraper.downloader import Downloader
+from scraper.html_getter import HTMLGetter
+
+
+class Command(NoArgsCommand):
+    help = "Print a cliche to the console."
+
+    def handle_noargs(self, **options):
+        page = PageHTML.objects.order_by('-tracking_number')[0]
+        initial = page.tracking_number
+        final = initial + 100
+
+        url_builder = URLBuilder(initial, final)
+        HTMLGetter(url_builder)()
