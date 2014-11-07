69ae74821e3c95f7b81d8d06f193d3dde7190830
Marcos Vanetta <marcosvanetta@gmail.com>
2014-11-06 18:00:18 -0600

    Show RNs

--- emission_events/emissions/views.py
+++ emission_events/emissions/views.py
@@ -1,5 +1,17 @@
+from django.db.models import Q
 from django.shortcuts import render
+from models import RegulatedEntity
 
+# validate len(q) > 2
 
-def search(request):
-    return render(request, 'search.html')
+def search_view(request):
+    regulated_entities = RegulatedEntity.objects.filter(\
+        Q(name__icontains=request.GET['q']) |\
+        Q(county__icontains=request.GET['q']) |\
+        Q(nearest_city__icontains=request.GET['q']) |\
+        Q(regulated_entity_rn_number__icontains=request.GET['q']))
+
+    return render(request, 'search.html', {
+        'q': request.GET['q'],
+        'regulated_entities': regulated_entities
+    })
