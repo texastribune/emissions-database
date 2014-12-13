09e067dc45cdea8601d5692af74e8f01fe48ba75
Marcos Vanetta <marcosvanetta@gmail.com>
2014-12-12 18:49:20 -0600

    Add logging to issued orders

--- emission_events/scraper/management/commands/downloadissuedorders.py
+++ emission_events/scraper/management/commands/downloadissuedorders.py
@@ -1,8 +1,11 @@
+import logging
 from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 from emissions.models import RegulatedEntity, IssuedOrder
 from scraper.issued_orders_scraper import IssuedOrdersScraper
 
+
+logger = logging.getLogger('emissions_downloader')
 
 class Command(BaseCommand):
     args = "--rn RN_NUMBER"
@@ -15,23 +18,33 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if options.get('rn') is None:
+
+            stored = 0
+            logger.info("Importing Issued Orders")
             for regulated_entity in RegulatedEntity.objects.all():
                 scraper = IssuedOrdersScraper(regulated_entity.regulated_entity_rn_number)
                 if scraper.records_found > 0:
                     orders_stored = self.store(scraper)
-                    print "%s: %i records found, %i records stored" %\
+                    stored += 1
+                    logger.info("%s: %i records found, %i records stored" %\
                         (scraper.rn_number, scraper.records_found, orders_stored)
+                    )
                 else:
-                    print "No records for %s" % scraper.rn_number
+                    logger.info("No records for %s" % scraper.rn_number)
+            logger.info("Issued orders importered!")
+            print "%i issued orders imported" % stored
 
         else:
             scraper = IssuedOrdersScraper(options.get('rn'))
             if scraper.records_found > 0:
                 orders_stored = self.store(scraper)
-                print "%s: %i records found, %i records stored" &\
+                logger.info("%s: %i records found, %i records stored" %\
                     (scraper.rn_number, scraper.records_found, orders_stored)
+                )
+
             else:
-                print "No records for %s" % scraper.rn_number
+                logger.info("No records for %s" % scraper.rn_number)
+            print "Done with %s" % options.get('rn')
 
     # Returns the number of orders created
     def store(self, scraper):
