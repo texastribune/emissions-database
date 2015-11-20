f8ec7e58fc5ee260695349953028c51d5071ca2d
Ryan Murphy <rdmurphy@users.noreply.github.com>
2015-11-20 12:34:12 -0600

    Revert "Refactor away the cd in the start script"
    This reverts commit 7513fc578f2bd39aa5acea691b524c4b86aa4083.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,7 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 
 # Run app
+cd emission_events
 exec gunicorn emission_events.wsgi:application \
   --workers 3 \
   --bind 0.0.0.0:8000 \
